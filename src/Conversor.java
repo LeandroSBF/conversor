1956f09f8728c902f20b78c4d97c3557c13427de
LeandroSBF <121291913+LeandroSBF@users.noreply.github.com>
2023-09-28 15:43:08 -0300

    Primer commit conversor

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.util.Arrays;
 
-import static javax.swing.JOptionPane.QUESTION_MESSAGE;
-
 public class Conversor {
     public Conversor() {
         String[] opciones = {"Conversor de Monedas", "Conversor de Temperaturas"};
